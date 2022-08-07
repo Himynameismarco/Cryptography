0dac18b135e64fa70833a21bd2e57fe1c494186a
Marco Zander <marcozander@Air-von-Marco.local>
2022-08-07 12:13:20 +0200

    Implemented Caesar Cipher

--- src/main/java/org/example/CryptographyMain.java
+++ src/main/java/org/example/CryptographyMain.java
@@ -3,5 +3,11 @@ package main.java.org.example;
 
 public class CryptographyMain {
 
+  public static void main(String[] args) {
+    CaesarImplementation caesarImplementation = new CaesarImplementation();
+    // caesarImplementation.makeAlphabetForKey(3);
+    //caesarImplementation.makeCaesar(        "At noon be in the conference room with your hat on for a surprise party. YELL LOUD!",15,false);
+    caesarImplementation.makeCaesarWithTwoKeys("At noon be in the conference room with your hat on for a surprise party. YELL LOUD!", 8, 21, false);
+  }
 }
 
